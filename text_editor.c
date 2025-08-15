7d5df15149ca35811ae3c2658e338787c7077ca4
endus <markus.gulla@gmail.com>
2025-08-15 14:05:30 -0600

    Update

--- text_editor.c
+++ text_editor.c
@@ -1,6 +1,92 @@
 // by Markus Gulla
 
-main()
-{
+// includes
+#include <ctype.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
 
+// defines
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// data
+struct termios orig_termios;
+
+// terminal
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void rawToggleDisable()
+{
+   if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {die("tcsetattr");}
+}
+
+void rawToggleEnable()
+{
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) {die("tcgetattr");}
+    atexit(rawToggleDisable);
+
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {die("tcsetattr");};
+}
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if(nread == -1 && errno != EAGAIN) {die("read");}
+    }
+}
+
+// input
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+    
+    switch(c)
+    {
+        case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
+// init
+int main()
+{
+    rawToggleEnable();
+
+    while (1)
+    {
+        char c = '\0';
+
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {die("read");}
+
+        read(STDIN_FILENO, &c, 1);
+
+        if(iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if(c == CTRL_KEY('q')){break;}
+    }
+    return 0;
 }
